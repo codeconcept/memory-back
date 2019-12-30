62b7fa3d0ac2ea1626425ba5e1e04d54ce5b8cfa
codeconcept <contact@codeconcept.fr>
2019-12-30 18:13:30 +0100

    Connecting to MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const connectDb = require("./utils/connectDb");
+
+const db = connectDb();
 
 app.get("/", (req, res) => {
   res
     .status(200)
     .json({ message: "It's alive", date: new Date().toISOString() });
+});
+
+app.get("/scores", (req, res) => {
+  if (db) {
+    res
+      .status(200)
+      .json({ message: "DB ready", date: new Date().toISOString() });
+  } else {
+    res.status(500).json({ message: "DB is NOT ready" });
+  }
 });
 
 const PORT = 5000;
---
+++ utils/connectDb.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDb = async () => {
+  const db = await mongoose.connect(process.env.MONGO_SRV, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  console.log("db connected: ", db);
+  return db;
+};
+
+module.exports = connectDb;
